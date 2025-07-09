20fe53c698f32db9e9abb8d496386ba454d954d8
Ping <dev.bastianabel@gmail.com>
2025-07-09 21:09:43 +0200

    fixed fullscreen function bug

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 function toggleFullscreen() {
-    let element = document.getElementById('container');
-    if(window.screenTop && window.screenY) {
-        element.requestFullscreen();
+    if(!document.fullscreenElement) {
+        openFullscreen();
     } else {
+        closeFullscreen();
+    }
+}
+
+function openFullscreen() {
+    let elem = document.getElementById('container');
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+    }    
+}
+
+function closeFullscreen() {
+    if (document.exitFullscreen) {
         document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
     }
 }
 
