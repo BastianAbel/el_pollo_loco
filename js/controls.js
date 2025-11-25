c82bfb8fe31c0afe6614fa007138ca32380c09b7
Ping <dev.bastianabel@gmail.com>
2025-11-25 12:58:50 +0100

    deleted comments in js/controls.js

--- js/controls.js
+++ js/controls.js
@@ -5,7 +5,6 @@ touchControls = {
   'btn-throw' : 'throw'
 }
 
-// muss dann mit dem unteren part noch verbunden werden
 function controlButtonDown(control) {
     world.keyboard[control] = true;
 }
@@ -36,29 +35,3 @@ function setTouchEndEventlistener(buttonId) {
     world.keyboard[touchControls[buttonId]] = false;
   })
 }
-
-// vorgenerierter code, wird noch verarbeitet zur button control
-{/* <button id="hold-button">Gedrückt halten</button>
-
-<script>
-const button = document.getElementById('hold-button');
-
-button.addEventListener('mousedown', () => {
-  console.log("Button wird gehalten");
-  // Starte z. B. eine Funktion oder setInterval
-});
-
-button.addEventListener('mouseup', () => {
-  console.log("Button wurde losgelassen");
-  // Stoppe z. B. die Funktion oder setInterval
-});
-
-// Optional für Touch-Geräte
-button.addEventListener('touchstart', () => {
-  console.log("Touch startet");
-});
-
-button.addEventListener('touchend', () => {
-  console.log("Touch endet");
-});
-</script> */}
